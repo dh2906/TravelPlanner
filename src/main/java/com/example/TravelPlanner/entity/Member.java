4162f2fc52d03946f2a58febe8c87aef06e4c9f5
Donghun <2dh2@naver.com>
2025-07-03 15:20:07 +0900

    feat. 멤버 엔티티 생성

---
+++ src/main/java/com/example/TravelPlanner/entity/Member.java
@@ -0,0 +1,40 @@
+package com.example.TravelPlanner.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "member")
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 50, nullable = false, unique = true)
+    private String email;
+
+    @Column(length = 255, nullable = false)
+    private String password;
+
+    @Column(length = 30, nullable = false)
+    private String name;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+    public enum Role {
+        USER, ADMIN
+    }
+}
