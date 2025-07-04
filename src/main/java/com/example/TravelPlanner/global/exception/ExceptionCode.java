b4bbb1d7b27c557093067e82865013dd5d252141
Donghun <2dh2@naver.com>
2025-07-04 17:06:58 +0900

    feat. 전역 예외 처리 기능 구현

---
+++ src/main/java/com/example/TravelPlanner/global/exception/ExceptionCode.java
@@ -0,0 +1,19 @@
+package com.example.TravelPlanner.global.exception;
+
+import lombok.Getter;
+
+@Getter
+public enum ExceptionCode {
+    MEMBER_NOT_FOUND(404, "회원 정보를 찾을 수 없습니다."),
+    EMAIL_ALREADY_EXISTS(409, "이미 사용 중인 이메일입니다."),
+    LOGIN_FAILED(400, "이메일 또는 비밀번호가 올바르지 않습니다."),
+    NO_ACCESS_TOKEN(401, "액세스 토큰이 존재하지 않습니다.");
+
+    private final int status;
+    private final String message;
+
+    ExceptionCode(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+}
---
+++ src/main/java/com/example/TravelPlanner/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package com.example.TravelPlanner.global.exception;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(CustomException.class)
+    public ResponseEntity<String> ExceptionHandle(CustomException ex) {
+        return ResponseEntity.status(
+                        ex.getExceptionCode()
+                                .getStatus()
+                )
+                .body(ex.getMessage());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<String>> ExceptionHandle(MethodArgumentNotValidException ex) {
+        List<String> errorMessages = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(DefaultMessageSourceResolvable::getDefaultMessage)
+                .collect(Collectors.toList());
+
+        return ResponseEntity.badRequest().body(errorMessages);
+    }
+}
