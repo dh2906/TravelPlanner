61da71522afb13fbb95b413b20db80fbb3a5a92e
Donghun <2dh2@naver.com>
2025-07-04 15:40:29 +0900

    feat. 로그인한 멤버의 자동 주입을 위한 ArgumentResolver 구현

---
+++ src/main/java/com/example/TravelPlanner/global/resolver/LoginMemberArgumentResolver.java
@@ -0,0 +1,64 @@
+package com.example.TravelPlanner.global.resolver;
+
+import com.example.TravelPlanner.entity.Member;
+import com.example.TravelPlanner.global.annotation.LoginMember;
+import com.example.TravelPlanner.global.jwt.JwtProvider;
+import com.example.TravelPlanner.repository.MemberRepository;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.MethodParameter;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+@Component
+@RequiredArgsConstructor
+public class LoginMemberArgumentResolver implements HandlerMethodArgumentResolver {
+    private final JwtProvider jwtProvider;
+    private final MemberRepository memberRepository;
+
+    @Override
+    public boolean supportsParameter(
+            MethodParameter parameter
+    ) {
+        return parameter.hasParameterAnnotation(LoginMember.class)
+                && parameter.getParameterType().equals(Member.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) throws Exception {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+
+        String token = getAccessTokenFromCookie(request);
+
+        if (token == null)
+            throw new RuntimeException("토큰이 존재하지 않습니다.");
+
+        Long memberId = jwtProvider.extractMemberId(token);
+
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new RuntimeException("해당 회원을 찾을 수 없습니다."));
+    }
+
+    public String getAccessTokenFromCookie(HttpServletRequest httpServletRequest) {
+        Cookie[] cookies = httpServletRequest.getCookies();
+
+        if (cookies == null)
+            return null;
+
+        for (Cookie cookie : cookies) {
+            if ("accessToken".equals(cookie.getName()))
+                return cookie.getValue();
+        }
+
+        return null;
+    }
+}
