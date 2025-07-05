256572a27f4daabd614e7d3abe4cfbdf4887d10f
Donghun <2dh2@naver.com>
2025-07-05 15:58:11 +0900

    feat. Plan 엔티티와 PlanDetail 엔티티 연관 관계 지정

--- src/main/java/com/example/TravelPlanner/entity/Plan.java
+++ src/main/java/com/example/TravelPlanner/entity/Plan.java
@@ -1,12 +1,17 @@
 package com.example.TravelPlanner.entity;
 
 import jakarta.persistence.*;
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "plan")
@@ -22,6 +27,9 @@ public class Plan {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
     private Member member;
+
+    @OneToMany(mappedBy = "plan", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<PlanDetail> details = new ArrayList<>();
 
     @Column(length = 100, nullable = false)
     private String title;
