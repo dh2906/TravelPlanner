f629abaefb5daf4ba667207524d2f27fa190ba13
Donghun <2dh2@naver.com>
2025-07-11 14:48:33 +0900

    feat. 날짜 및 시간 범위 검증용 어노테이션, Validator 구현

---
+++ src/main/java/com/example/TravelPlanner/global/annotation/ValidDateTimeRange.java
@@ -0,0 +1,17 @@
+package com.example.TravelPlanner.global.annotation;
+
+import com.example.TravelPlanner.global.validation.DateTimeRangeValidator;
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+
+import java.lang.annotation.*;
+
+@Documented
+@Constraint(validatedBy = DateTimeRangeValidator.class)
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ValidDateTimeRange {
+    String message() default "종료 시점은 시작 시점보다 이후여야 합니다.";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+}
---
+++ src/main/java/com/example/TravelPlanner/global/validation/DateTimeRangeValidator.java
@@ -0,0 +1,45 @@
+package com.example.TravelPlanner.global.validation;
+
+import com.example.TravelPlanner.global.annotation.*;
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+import java.lang.reflect.Field;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public class DateTimeRangeValidator implements ConstraintValidator<ValidDateTimeRange, Object> {
+    @Override
+    public boolean isValid(Object o, ConstraintValidatorContext constraintValidatorContext) {
+        LocalDate startDate = null;
+        LocalDate endDate = null;
+        LocalTime startTime = null;
+        LocalTime endTime = null;
+
+        try {
+            for (Field field : o.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+
+                if (field.isAnnotationPresent(StartDate.class)) {
+                    startDate = (LocalDate) field.get(o);
+                } else if (field.isAnnotationPresent(EndDate.class)) {
+                    endDate = (LocalDate) field.get(o);
+                } else if (field.isAnnotationPresent(StartTime.class)) {
+                    startTime = (LocalTime) field.get(o);
+                } else if (field.isAnnotationPresent(EndTime.class)) {
+                    endTime = (LocalTime) field.get(o);
+                }
+            }
+        } catch (IllegalAccessException e) {
+            return false;
+        }
+
+        if (startDate != null && endDate != null) {
+            return startDate.isBefore(endDate);
+        } else if (startTime != null & endTime != null) {
+            return startTime.isBefore(endTime);
+        }
+
+        return false;
+    }
+}
