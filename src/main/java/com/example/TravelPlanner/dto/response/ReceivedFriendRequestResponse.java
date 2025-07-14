817d050f77c55077aa472dfc29d592cbd347d48d
Donghun <2dh2@naver.com>
2025-07-14 14:10:12 +0900

    feat. 친구 요청 id도 반환하도록 필드 추가

--- src/main/java/com/example/TravelPlanner/dto/response/ReceivedFriendRequestResponse.java
+++ src/main/java/com/example/TravelPlanner/dto/response/ReceivedFriendRequestResponse.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 
 @Builder
 public record ReceivedFriendRequestResponse(
+        Long id,
         Long senderId,
         String senderName
 ) {
     public static ReceivedFriendRequestResponse fromEntity(FriendRequest friendRequest) {
         return ReceivedFriendRequestResponse.builder()
+                .id(friendRequest.getId())
                 .senderId(friendRequest.getSender().getId())
                 .senderName(friendRequest.getSender().getName())
                 .build();
--- src/main/java/com/example/TravelPlanner/dto/response/SentFriendRequestResponse.java
+++ src/main/java/com/example/TravelPlanner/dto/response/SentFriendRequestResponse.java
@@ -5,11 +5,13 @@ import lombok.Builder;
 
 @Builder
 public record SentFriendRequestResponse(
+        Long id,
         Long receiverId,
         String receiverName
 ) {
     public static SentFriendRequestResponse fromEntity(FriendRequest friendRequest) {
         return SentFriendRequestResponse.builder()
+                .id(friendRequest.getId())
                 .receiverId(friendRequest.getReceiver().getId())
                 .receiverName(friendRequest.getReceiver().getName())
                 .build();
