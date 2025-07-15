262db64e3b745cd7618ac28ac29ad9100b93fc22
Donghun <2dh2@naver.com>
2025-07-15 15:22:18 +0900

    refactor. 체크리스트 항목 전체 삭제 시 DELETE 쿼리가 N개 나가는 문제 해결

--- src/main/java/com/example/TravelPlanner/repository/ChecklistItemRepository.java
+++ src/main/java/com/example/TravelPlanner/repository/ChecklistItemRepository.java
@@ -3,11 +3,19 @@ package com.example.TravelPlanner.repository;
 import com.example.TravelPlanner.entity.ChecklistItem;
 import com.example.TravelPlanner.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ChecklistItemRepository extends JpaRepository<ChecklistItem, Long> {
     public List<ChecklistItem> findAllByMemberId(Long memberId);
 
-    public void deleteAllByMemberId(Long memberId);
+    @Modifying(clearAutomatically = true)
+    @Query(value = """
+            DELETE FROM ChecklistItem c
+            WHERE c.member.id = :memberId
+            """)
+    public void deleteAllByMemberId(@Param("memberId") Long memberId);
 }
