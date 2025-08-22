54cc550e6785725c5666a01baef952756f92fb25
Donghun <2dh2@naver.com>
2025-08-22 16:52:18 +0900

    feat. CORS 정책 설정

--- src/main/java/com/example/TravelPlanner/global/config/WebConfig.java
+++ src/main/java/com/example/TravelPlanner/global/config/WebConfig.java
@@ -5,6 +5,7 @@ import com.example.TravelPlanner.global.web.resolver.LoginMemberArgumentResolver
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -25,5 +26,15 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(planAccessInterceptor)
             .addPathPatterns("/api/plans/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+            .allowedOrigins("http://localhost:3000")
+            .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
+            .allowedHeaders("Authorization", "Content-Type")
+            .allowCredentials(true)
+            .maxAge(3600);
     }
 }
